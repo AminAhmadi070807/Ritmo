613643435227b70b5cdb6f2314ce96501cc527b6
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-11-18 11:26:24 +0330

    chore base file

---
+++ BackEnd/src/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+module.exports = app
