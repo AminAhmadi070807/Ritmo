0cfe3091a982695c1f6a88ae675216ff945f8312
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-11-19 08:35:07 +0330

    feat(database): create user table

--- BackEnd/src/modules/v1/auth/auth.controller.js
+++ BackEnd/src/modules/v1/auth/auth.controller.js
@@ -1,5 +1,7 @@
 "use strict"
 
+const userModel = require('../users/user.model')
+
 module.exports.viewRegister = (req, res ) => res.render('register.ejs')
 
 module.exports.register = async (req, res, next) => {
---
+++ BackEnd/src/modules/v1/users/user.model.js
@@ -0,0 +1,53 @@
+"use strict"
+
+const { DataTypes } = require("sequelize");
+const mysql = require('../../../database/MySql/db')
+
+const options = {}
+
+module.exports = mysql.define("User", {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    family: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            is: /^\w+$/
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    profile: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: '/uploads/profiles/profile.png'
+    },
+    bio: {
+        type: DataTypes.TEXT('tiny'),
+        allowNull: false,
+        validate: {
+            len: [0, 100]
+        }
+    },
+    isNotification: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+}, options)
