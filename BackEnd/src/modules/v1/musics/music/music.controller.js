48362aff7b365bdc819eb7b7539bf18741c859d8
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-11-25 08:10:33 +0330

    chore(upload): update upload multer

--- BackEnd/src/modules/v1/musics/music/music.controller.js
+++ BackEnd/src/modules/v1/musics/music/music.controller.js
@@ -1,10 +1,14 @@
 "use strict"
 
+const musicModel = require('./music.model')
+
 module.exports.create = async (req, res, next) => {
     try {
-        const { channel, tags, album, artist, category } = req.body
+        const user = req.user;
 
+        const { channel, tags, album, artist, category } = req.body;
 
+        console.log(channel, tags, album, artist, category, user);
     }
     catch (error) {
         next(error)
