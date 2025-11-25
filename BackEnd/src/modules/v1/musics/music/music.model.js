f92d354da9fcfb3f43c7b387bffd709dbe229d57
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-11-25 07:28:05 +0330

    feat(music): add music model

---
+++ BackEnd/src/modules/v1/musics/music/music.model.js
@@ -0,0 +1,38 @@
+"use strict"
+
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+    music: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    poster: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    channel: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    category: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    artist: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    album: {
+        type: String,
+        required: true,
+        trim: true,
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Music", schema);
