f7bf3693ac86782a5cc2e24351b5c4496db1d835
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-11-25 07:52:35 +0330

    chore(music): add upload music

--- BackEnd/src/modules/v1/musics/music/music.route.js
+++ BackEnd/src/modules/v1/musics/music/music.route.js
@@ -10,6 +10,8 @@ const controller = require('./music.controller')
 const validator = require('../../../../middlewares/validate.middleware')
 const authGuard = require('../../../../middlewares/guard/auth.guard')
 
-router.route('/').post(authGuard, validator.createMusicValidator, controller.create)
+const upload = multer({ storage: diskStorage('music'), limits: { fileSize: 1024 * 1024 * 5 } });
+
+router.route('/').post(authGuard, upload.fields([{ name: 'music', maxCount: 1 }, { name: 'poster', maxCount: 1 }]), validator.createMusicValidator, controller.create)
 
 module.exports = router;
