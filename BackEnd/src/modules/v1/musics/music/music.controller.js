20db4e2ddabcdce87e2c7f9e3b5710806f19277b
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-11-25 08:30:28 +0330

    chore(music): update music controller

--- BackEnd/src/modules/v1/musics/music/music.controller.js
+++ BackEnd/src/modules/v1/musics/music/music.controller.js
@@ -2,13 +2,19 @@
 
 const musicModel = require('./music.model')
 
+const fileFormat = {
+    "music": ["audio/mpeg", "audio/wav", "audio/x-wav", "audio/ogg", "audio/opus", "audio/mp4", "audio/x-m4a", "audio/flac"],
+    "image": ["image/jpeg", "image/jpg", "image/png", "image/webp", "image/gif", "image/svg+xml"]
+}
+
+// todo validate channel
 module.exports.create = async (req, res, next) => {
     try {
         const user = req.user;
 
         const { channel, tags, album, artist, category } = req.body;
 
-        console.log(channel, tags, album, artist, category, user);
+        const { music, poster } = req.files;
     }
     catch (error) {
         next(error)
--- BackEnd/src/modules/v1/musics/music/music.route.js
+++ BackEnd/src/modules/v1/musics/music/music.route.js
@@ -10,7 +10,7 @@ const controller = require('./music.controller')
 const validator = require('../../../../middlewares/validate.middleware')
 const authGuard = require('../../../../middlewares/guard/auth.guard')
 
-const upload = multer({ storage: diskStorage('music'), limits: { fileSize: 1024 * 1024 * 5 } });
+const upload = multer({ storage: diskStorage('music'), limits: { fileSize: 1024 * 1024 * 100 } });
 
 router.route('/').post(authGuard, upload.fields([{ name: 'music', maxCount: 1 }, { name: 'poster', maxCount: 1 }]), validator.createMusicValidator, controller.create)
 
