b0a6e02cf67cab02eb21a6c315164ab1e3cfa1c9
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-11-25 07:35:45 +0330

    chore(music): update music router

--- BackEnd/src/modules/v1/musics/music/music.route.js
+++ BackEnd/src/modules/v1/musics/music/music.route.js
@@ -1,8 +1,14 @@
 "use strict"
 
 const express = require("express");
+const multer = require("multer");
+const diskStorage = require('../../../../utils/upload.util')
+
 const router = express.Router();
 
+const controller = require('./music.controller')
+const validator = require('../../../../middlewares/validate.middleware')
+const authGuard = require('../../../../middlewares/guard/auth.guard')
 
 
 module.exports = router;
