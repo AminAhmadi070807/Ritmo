6138bd87efceccb334f0a97cf619ae9aa0dbee5d
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-11-27 15:07:42 +0330

    feat(album): add album route

---
+++ BackEnd/src/modules/v1/musics/album/album.route.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const express = require("express");
+const router = express.Router();
+
+const multer = require("multer");
+const diskStorage = require('../../../../utils/upload.util')
+
+const upload = multer({ storage: diskStorage("albums") })
+
+const validator = require('../../../../middlewares/validate.middleware')
+const authGuard = require('../../../../middlewares/guard/auth.guard')
+const roleGuard = require('../../../../middlewares/guard/role.guard')
+
+router.route('/').post(authGuard, roleGuard("ARTIST"), upload.single("cover"), validator.albumValidator)
+
+module.exports = router;
