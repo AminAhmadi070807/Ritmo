7e87e2df282b526c4ae98f4a7d0c6805edd15750
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-11-29 12:26:51 +0330

    feat(playlist): add remove playlist

--- BackEnd/src/modules/v1/musics/playlists/playlist.controller.js
+++ BackEnd/src/modules/v1/musics/playlists/playlist.controller.js
@@ -3,6 +3,7 @@
 const playlistModel = require('./playlist.model')
 const response = require('../../../../helpers/response.helper')
 const deleteFile = require("../../../../utils/delete.file");
+const {isValidObjectId} = require("mongoose");
 
 const fileFormat = ["image/jpeg", "image/jpg", "image/png", "image/webp", "image/gif", "image/svg+xml"]
 
@@ -56,4 +57,26 @@ module.exports.getAll = async (req, res, next) => {
     }
 }
 
-module.exports.remove = async (req, res, next) => {}
+module.exports.remove = async (req, res, next) => {
+    try {
+        const user = req.user;
+        const { id } = req.params
+
+        if (!isValidObjectId(id)) return response(res, 400, "playlist id is not correct.")
+
+        const playlist = await playlistModel.findById(id).lean()
+
+        if (!playlist) return response(res, 404, "playlist not found. or has already been removed.")
+
+        if (!user.role.includes("ADMIN") || (user.uuid.toString() !== playlist.user)) return response(res, 400, "you not access to removed album.")
+
+        await playlist.findByIdAndDelete(id).lean()
+
+        await deleteFiles(['BackEnd/public' + playlist.cover])
+
+        return response(res, 200, "deleted playlist successfully.")
+    }
+    catch (error) {
+        next(error)
+    }
+}
