8d649f9e1d06a9aeb4ec6c51dc05836d8a3743a7
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-01 09:12:40 +0330

    chore(category): add description to category model

--- BackEnd/src/modules/v1/musics/categories/category.model.js
+++ BackEnd/src/modules/v1/musics/categories/category.model.js
@@ -21,5 +21,9 @@ module.exports = database.define("MusicCategory", {
     icon: {
         type: DataTypes.STRING,
         allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
     }
 }, { timestamps: true })
