2ed67a2c15295648feda861e122e67053c200ada
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-01 09:13:45 +0330

    chore(category): update category schema

--- BackEnd/src/modules/v1/musics/categories/category.schema.js
+++ BackEnd/src/modules/v1/musics/categories/category.schema.js
@@ -4,6 +4,7 @@ const Joi = require('joi')
 
 module.exports = Joi.object().keys({
     title: Joi.string().max(50).required(),
+    description: Joi.string().min(100).required(),
     href: Joi.string().max(50).required(),
     icon: Joi.string().required(),
 })
