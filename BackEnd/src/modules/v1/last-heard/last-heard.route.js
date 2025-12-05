a1fecb20ea0ebd2cd837acba4a9413dd4394803c
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-05 14:26:15 +0330

    add last-heard to route

--- BackEnd/src/modules/v1/last-heard/last-heard.route.js
+++ BackEnd/src/modules/v1/last-heard/last-heard.route.js
@@ -6,5 +6,6 @@ const router = express.Router();
 const controller = require('./last-heard.controller')
 const authGuard = require('../../../middlewares/guard/auth.guard')
 
+router.route('/:id').post(authGuard, controller.add)
 
 module.exports = router;
