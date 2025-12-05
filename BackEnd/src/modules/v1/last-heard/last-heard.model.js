c08e412f83b402d50eac2aff453ada5b7cff3223
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-05 14:27:46 +0330

    chore(last-heard): update last-heard model

--- BackEnd/src/modules/v1/last-heard/last-heard.model.js
+++ BackEnd/src/modules/v1/last-heard/last-heard.model.js
@@ -11,6 +11,11 @@ const schema = new mongoose.Schema({
         type: String,
         required: true,
         trim: true
+    },
+    time: {
+        type: Number,
+        required: true,
+        default: 0
     }
 }, { timestamps: true });
 
