883be7c7a718d1a841db8de16aca8cd5adbcb76c
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-05 20:33:51 +0330

    chore: update albumDetail & playlistDetails

--- BackEnd/public/scripts/albums/albumDetail.js
+++ BackEnd/public/scripts/albums/albumDetail.js
@@ -2,16 +2,14 @@
 
 const $ = document;
 
-const toggleMenu = document.getElementById('toggle-menu')
+const togglePlaylistMenus = document.querySelectorAll("#toggle-menu")
 
-
-toggleMenu.addEventListener('click', (event) => {
+togglePlaylistMenus.forEach(btn => {
+    btn.addEventListener("click", (event) => {
         let musicList = event.currentTarget.parentElement.parentElement;
-        let musicList_2 = musicList.parentElement.querySelector("#album-menu.opacity-100");
+        let musicList_2 = musicList.parentElement.querySelector("#playlist-menu.opacity-100");
 
-    console.log(musicList_2);
-
-        let musicListMenu = musicList.querySelector("#album-menu");
+        let musicListMenu = musicList.querySelector("#playlist-menu");
         musicListMenu.classList.toggle("hidden");
         musicListMenu.classList.toggle("opacity-0");
         musicListMenu.classList.toggle("opacity-100");
@@ -19,11 +17,10 @@ toggleMenu.addEventListener('click', (event) => {
             musicList_2.classList.add("hidden");
             musicList_2.classList.add("opacity-0");
             musicList_2.classList.remove("opacity-100");
-        }
-        else {
+        } else {
             musicListMenu.classList.remove("hidden");
             musicListMenu.classList.remove("opacity-0");
             musicListMenu.classList.add("opacity-100");
         }
-
     })
+})
--- BackEnd/public/scripts/playlist/playlistDetails.js
+++ BackEnd/public/scripts/playlist/playlistDetails.js
@@ -3,23 +3,25 @@
 const playlist = document.getElementById('playlist')
 
 
-const togglePlaylistMenu = document.getElementById("toggle-menu")
+const togglePlaylistMenus = document.querySelectorAll("#toggle-menu")
 
-togglePlaylistMenu.addEventListener("click", (event) => {
-    let musicList = event.currentTarget.parentElement.parentElement;
-    let musicList_2 = musicList.parentElement.querySelector("#playlist-menu.opacity-100");
+togglePlaylistMenus.forEach(btn => {
+    btn.addEventListener("click", (event) => {
+        let musicList = event.currentTarget.parentElement.parentElement;
+        let musicList_2 = musicList.parentElement.querySelector("#playlist-menu.opacity-100");
 
-    let musicListMenu = musicList.querySelector("#playlist-menu");
-    musicListMenu.classList.toggle("hidden");
-    musicListMenu.classList.toggle("opacity-0");
-    musicListMenu.classList.toggle("opacity-100");
-    if (musicList_2) {
-        musicList_2.classList.add("hidden");
-        musicList_2.classList.add("opacity-0");
-        musicList_2.classList.remove("opacity-100");
-    } else {
-        musicListMenu.classList.remove("hidden");
-        musicListMenu.classList.remove("opacity-0");
-        musicListMenu.classList.add("opacity-100");
-    }
+        let musicListMenu = musicList.querySelector("#playlist-menu");
+        musicListMenu.classList.toggle("hidden");
+        musicListMenu.classList.toggle("opacity-0");
+        musicListMenu.classList.toggle("opacity-100");
+        if (musicList_2) {
+            musicList_2.classList.add("hidden");
+            musicList_2.classList.add("opacity-0");
+            musicList_2.classList.remove("opacity-100");
+        } else {
+            musicListMenu.classList.remove("hidden");
+            musicListMenu.classList.remove("opacity-0");
+            musicListMenu.classList.add("opacity-100");
+        }
+    })
 })
