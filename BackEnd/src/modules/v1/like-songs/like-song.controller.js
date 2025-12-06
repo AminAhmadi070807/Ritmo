6f97408ed701466ff9673482adb677f3620ef07e
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-06 11:20:19 +0330

    feat(like-song): add toggleLike song

---
+++ BackEnd/src/modules/v1/like-songs/like-song.controller.js
@@ -0,0 +1,31 @@
+"use strict"
+
+const likeSongModel = require('./like-song.model')
+const {isValidObjectId} = require("mongoose");
+const response = require('../../../helpers/response.helper')
+
+module.exports.toggleLike = async (req, res, next) => {
+    try {
+        const user = req.user;
+        const { id } = req.params
+
+        if (!isValidObjectId(id)) return response(res, 400, 'music id is not correct')
+
+        const isExistMusicLike = await likeSongModel.findOne({ user: user.uuid, music: id }).lean()
+
+        if (isExistMusicLike) {
+            await likeSongModel.findByIdAndDelete(isExistMusicLike._id)
+            return response(res, 204, 'music dis liked successfully')
+        }
+
+        await likeSongModel.create({
+            user: user.uuid,
+            music: id,
+        })
+
+        return response(res, 201, 'music liked successfully')
+    }
+    catch (error) {
+        next(error)
+    }
+}
