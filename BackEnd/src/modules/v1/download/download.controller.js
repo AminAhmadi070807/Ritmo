a17ac8bfe3ed70b082eba9e84ff555e0991c7488
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-07 08:01:43 +0330

    chore(download): update download controller

---
+++ BackEnd/src/modules/v1/download/download.controller.js
@@ -0,0 +1,53 @@
+"use strict"
+
+const downloadModel = require('./download.model')
+const musicModel = require('../musics/music/music.model')
+const {isValidObjectId} = require("mongoose");
+const response = require('../../../helpers/response.helper')
+
+module.exports.toggleLike = async (req, res, next) => {
+    try {
+        const user = req.user;
+        const { id } = req.params
+
+        if (!isValidObjectId(id)) return response(res, 400, 'music id is not correct')
+
+        const isExistMusic = await musicModel.findById(id).lean()
+
+        if (!isExistMusic) return response(res, 404, 'music not found. or has already been removed.')
+
+        const isExistMusicLike = await downloadModel.findOne({ user: user.uuid, music: id }).lean()
+
+        if (isExistMusicLike) {
+            await likeSongModel.findByIdAndDelete(isExistMusicLike._id)
+            return response(res, 200, 'music dis liked successfully')
+        }
+
+        await downloadModel.create({
+            user: user.uuid,
+            music: id,
+        })
+
+        return response(res, 201, 'music downloaded successfully')
+    }
+    catch (error) {
+        next(error)
+    }
+}
+
+module.exports.userDownloads = async (req, res, next) => {
+    try {
+        const { limit = 100, page = 1 } = req.query;
+
+        const user = req.user;
+
+        const numberOfUserDownloads = await downloadModel.countDocuments({})
+
+        const downloads = await downloadModel.find({user: user.uuid}, 'music').limit(+page * +limit).lean().sort({ updatedAt: -1 }).populate('music')
+
+        return response(res, 200, null, { downloads, count: numberOfUserDownloads });
+    }
+    catch (error) {
+        next(error)
+    }
+}
