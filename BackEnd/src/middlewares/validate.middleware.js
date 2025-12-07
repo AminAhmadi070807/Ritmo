6a595cea7c4cc37005a910c893d4f663cc545bb0
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-07 14:18:00 +0330

    chore: validation plan

--- BackEnd/src/middlewares/validate.middleware.js
+++ BackEnd/src/middlewares/validate.middleware.js
@@ -5,6 +5,7 @@ const categorySchema = require('../modules/v1/musics/categories/category.schema'
 const createMusicSchema = require('../modules/v1/musics/music/music.schema')
 const genreSchema = require('../modules/v1/musics/genre/genre.schema')
 const { createAlbumSchema } = require('../modules/v1/musics/album/album.schema')
+const planSchema = require('../modules/v1/plan/plan.schema')
 
 module.exports.authRegisterValidation = async (req, res, next) => {
     try {
@@ -64,6 +65,17 @@ module.exports.genreValidator = async (req, res, next) => {
 module.exports.createAlbumValidator = async (req, res, next) => {
     try {
         await createAlbumSchema.validateAsync({ ...req.body })
+
+        next()
+    }
+    catch (error) {
+        next(error)
+    }
+}
+
+module.exports.planValidator = async (req, res, next) => {
+    try {
+        await planSchema.validateAsync({ ...req.params })
 
         next()
     }
--- BackEnd/src/modules/v1/plan/plan.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const Joi = require('joi')
-
-module.exports = Joi.object().keys({
-    price: Joi.number().integer().required(),
-    month: Joi.number().integer().required(),
-})
