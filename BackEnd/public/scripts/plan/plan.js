ee33a4be4760a94b0ab37f27d2f24c53ba18261d
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-07 12:11:55 +0330

    add plan script file

---
+++ BackEnd/public/scripts/plan/plan.js
@@ -0,0 +1,20 @@
+"use strict"
+
+
+const questionRitmoPlus = document.querySelectorAll("#questions-ritmo-plus");
+
+questionRitmoPlus.forEach((questionContainer) => {
+    questionContainer.addEventListener("click", () => {
+    if (!questionContainer.querySelector("p.hidden")) {
+        questionContainer.classList.add("bg-transparent-1/75");
+        questionContainer.classList.remove("ritmo-questions");
+        questionContainer.querySelector("#answer").classList.add("hidden");
+        questionContainer.querySelector("#arrowDown").classList.remove("rotate-180");
+    } else {
+        questionContainer.classList.remove("bg-transparent-1/75");
+        questionContainer.classList.add("ritmo-questions");
+        questionContainer.querySelector("#answer").classList.remove("hidden");
+        questionContainer.querySelector("#arrowDown").classList.add("rotate-180");
+    }
+  });
+});
