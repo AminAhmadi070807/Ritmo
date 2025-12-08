05cb2ac8e5db205fd01b7fe84d56defcfcd1f13e
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-08 04:50:58 +0330

    chore(plan): update plan schema

--- BackEnd/src/modules/v1/plan/plan.schema.js
+++ BackEnd/src/modules/v1/plan/plan.schema.js
@@ -5,4 +5,5 @@ const Joi = require('joi')
 module.exports = Joi.object().keys({
     price: Joi.number().integer().required(),
     month: Joi.number().integer().required(),
+    description: Joi.array().items(Joi.string().required()),
 })
