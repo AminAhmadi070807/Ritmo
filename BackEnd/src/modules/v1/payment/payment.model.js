a7c44f4653cfeb2ddcab453781fb76d100cc6733
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-08 09:22:01 +0330

    chore(payment): setup payment model

---
+++ BackEnd/src/modules/v1/payment/payment.model.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    user: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    model: {
+        type: String,
+        required: true,
+        enum: ["PLAN"]
+    },
+    objectID: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    }
+}, {timestamp: true})
+
+module.exports = mongoose.model('Payment', schema);
