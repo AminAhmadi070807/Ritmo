04a02a7b6408fe97c30fbeb90f3f80b364a87385
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-08 05:58:48 +0330

    add route to plan route

--- BackEnd/src/modules/v1/plan/plan.route.js
+++ BackEnd/src/modules/v1/plan/plan.route.js
@@ -10,4 +10,6 @@ const validator = require('../../../middlewares/validate.middleware')
 
 router.route('/').post(authGuard, roleGuard("ADMIN"), validator.planValidator , controller.create).get(controller.getAll)
 
+router.route('/:id').delete(authGuard, roleGuard("ADMIN"), validator.planValidator, controller.remove)
+
 module.exports = router;
