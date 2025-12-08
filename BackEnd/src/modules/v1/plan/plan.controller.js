382227c73150ccebfa09932c685f6b5920c5a4da
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-08 05:57:58 +0330

    feat(plan): remove plan feature

--- BackEnd/src/modules/v1/plan/plan.controller.js
+++ BackEnd/src/modules/v1/plan/plan.controller.js
@@ -2,6 +2,7 @@
 
 const planModel = require('./plan.model')
 const response = require('../../../helpers/response.helper')
+const {isValidObjectId} = require("mongoose");
 
 module.exports.create = async (req, res, next) => {
     try {
@@ -25,6 +26,23 @@ module.exports.getAll = async (req, res, next) => {
         const plans = await planModel.find({}).lean()
 
         return response(res, 200, null, { plans })
+    }
+    catch (error) {
+        next(error)
+    }
+}
+
+module.exports.remove = async (req, res, next) => {
+    try {
+        const { id } = req.params
+
+        if (!isValidObjectId(id)) return response(res, 400, "plan id is not correct.")
+
+        const plan = await planModel.findByIdAndDelete(id).lean()
+
+        if (!plan) return response(res, 404, "plan not found")
+
+        return response(res, 204)
     }
     catch (error) {
         next(error)
