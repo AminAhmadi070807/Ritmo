82703fec693b13b9e624b5888ef9f3046b317fd0
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-09 09:15:49 +0330

    feat(suggestion): add base suggestion music

---
+++ BackEnd/src/modules/v1/suggestion/suggestion.controller.js
@@ -0,0 +1,55 @@
+"use strict"
+
+const musicModel = require('../musics/music/music.model')
+const likeModel = require('../like-songs/like-song.model')
+const lastHeardModel = require('../last-heard/last-heard.model')
+const response = require('../../../helpers/response.helper')
+
+module.exports = async (req, res, next) => {
+    try {
+        const user = req.user;
+
+        const musics = await musicModel.find({}).sort({ createdAt: -1 }).lean()
+        const userLikeMusics = await likeModel.find({ user: user.uuid }).lean().sort({ createdAt: -1 }).populate('music')
+        const lastHeardMusics = await lastHeardModel.find({ user: user.uuid, percent: { $gte : 50 } }).sort({ createdAt: -1 }).lean().populate('music')
+
+        const suggestionTagArray = []
+
+        for (const likeMusic of userLikeMusics) {
+            for (const tag of likeMusic.music.tags) {
+                const existing = suggestionTagArray.find(t => Object.keys(t)[0] === tag.trim());
+
+                if (!existing) {
+                    suggestionTagArray.push({
+                        [tag.trim()]: { score: 3, title: tag.trim() }
+                    });
+                }else existing[tag.trim()].score += 3;
+            }
+        }
+
+        for (const lastHeard of lastHeardMusics) {
+            for (const tag of lastHeard.music.tags) {
+                const existing = suggestionTagArray.find(t => Object.keys(t)[0] === tag.trim());
+
+                if (!existing) suggestionTagArray.push({
+                        [tag.trim()] : { score: Math.ceil(lastHeard.numberOfPlay * lastHeard.percent / 100), title: tag.trim() }
+                });
+                else {
+                    existing[tag.trim()].score += Math.ceil(lastHeard.numberOfPlay * lastHeard.percent / 100)
+                }
+            }
+        }
+
+        const suggestionSortArray = suggestionTagArray.sort((a, b) => {
+            const scoreA = Object.values(a)[0].score;
+            const scoreB = Object.values(b)[0].score;
+            return scoreB - scoreA; // نزولی
+        });
+
+
+        return response(res, 200, null, { suggestionSortArray })
+    }
+    catch (error) {
+        next(error)
+    }
+}
--- BackEnd/src/modules/v1/suggestion/suggestion.route.js
+++ BackEnd/src/modules/v1/suggestion/suggestion.route.js
@@ -6,4 +6,6 @@ const router = express.Router();
 const controller = require('./suggestion.controller')
 const authGuard = require('../../../middlewares/guard/auth.guard')
 
+router.get('/', authGuard, controller)
+
 module.exports = router;
