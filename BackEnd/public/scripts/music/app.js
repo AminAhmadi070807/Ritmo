141fbbb18bb57e70f85e96ce7359c2920494ce86
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-10 05:44:25 +0330

    add view number of user download musics to client

--- BackEnd/public/scripts/music/app.js
+++ BackEnd/public/scripts/music/app.js
@@ -295,8 +295,14 @@ const formatTime = (seconds) => {
         const likeSongResponse = await fetch('/api/v1/musics/likeSongs')
         const data = await likeSongResponse.json()
 
+        const downloadResponse = await fetch('/api/v1/musics/downloads')
+        const downloadData = await downloadResponse.json()
+
         document.getElementById('number-of-user-like-song').setAttribute('value', data.data.numberOfUserLikeSong)
         document.getElementById('number-of-user-like-song').innerText = data.data.numberOfUserLikeSong
+
+        document.getElementById('number-of-download-music').setAttribute('value', downloadData.data.count)
+        document.getElementById('number-of-download-music').innerText = downloadData.data.count
     }
     catch (error) {
         console.log(error);
