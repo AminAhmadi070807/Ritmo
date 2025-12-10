2b91bae1da86846f5dc3a567bb1a2943fe3f26a7
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-10 09:32:02 +0330

    update client player

--- BackEnd/public/scripts/music/app.js
+++ BackEnd/public/scripts/music/app.js
@@ -65,11 +65,18 @@ const formatTime = (seconds) => {
 
         musicBtn.forEach(music => {
             music.addEventListener('click', async () => {
+                const refresh = await fetch('/api/v1/auth/refresh')
+                if (refresh.status !== 200) return location.href = '/auth/send'
+
                 const response = await fetch(`/api/v1/musics/${music.getAttribute('music-id')}`)
                 const data = await response.json();
 
+                document.getElementById('music-player-box').classList.remove("hidden")
+
                 document.getElementById('music-box').src = data.data.poster
                 audio.src = data.data.music
+                audio.currentTime = 0
+                rangePlayer.style.width = 0 + "%"
                 audio.setAttribute('audio-id', data.data._id)
             })
         })
