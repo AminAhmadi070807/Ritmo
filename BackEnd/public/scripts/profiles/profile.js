cf68bd9e243824ded2a8d29e01512471d30575f4
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-11 08:37:53 +0330

    refactor(profile): update profile client

--- BackEnd/public/scripts/profiles/profile.js
+++ BackEnd/public/scripts/profiles/profile.js
@@ -15,6 +15,18 @@
         document.getElementById('profile-title').innerText = data.data.fullName
 
         document.getElementById('profile-role').innerText = `${data.data.role[0] === "ADMIN" ? "ادمین" : data.data.role[0] === "ARTIST" ? "هنرمند" : "کاربر"}`
+
+        const responseLikeSong = await fetch('/api/v1/musics/likeSongs')
+        const dataLikeSong = await responseLikeSong.json()
+
+        document.getElementById('profile-number-of-heart-music').innerText = dataLikeSong.data.numberOfUserLikeSong
+        document.getElementById('profile-number-of-heart-music').value = dataLikeSong.data.numberOfUserLikeSong
+
+        const responseDownload = await fetch('/api/v1/musics/downloads')
+        const dataDownload = await responseDownload.json()
+
+        document.getElementById('profile-number-of-download').innerText = dataDownload.data.count
+        document.getElementById('profile-number-of-download').value = dataDownload.data.count
     }
     catch (error) {
         console.error(error);
