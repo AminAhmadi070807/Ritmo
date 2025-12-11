a4fd3c591b6b2134345c8da32e643cc88936aed5
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-11 19:44:38 +0330

    refactor(users): add validator to route

--- BackEnd/src/modules/v1/users/user.route.js
+++ BackEnd/src/modules/v1/users/user.route.js
@@ -7,7 +7,7 @@ const controller = require('./user.controller')
 const validator = require('../../../middlewares/validate.middleware')
 const authGuard = require('../../../middlewares/guard/auth.guard')
 
-router.patch('/update', authGuard, controller.update)
+router.patch('/update', authGuard, validator.userUpdateInfoValidator, controller.update)
 router.get('/Me', authGuard, controller.getMe)
 router.get('/logout', authGuard, controller.logout)
 
