6b5c921e2cba07c7ec619bcd8e65bedc9f45dc65
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-12 12:46:45 +0330

    chore(FAQ): add All faq route

--- BackEnd/src/modules/v1/FAQ/faq.route.js
+++ BackEnd/src/modules/v1/FAQ/faq.route.js
@@ -8,7 +8,7 @@ const validator = require('../../../middlewares/validate.middleware')
 const authGuard = require('../../../middlewares/guard/auth.guard');
 const roleGuard = require('../../../middlewares/guard/role.guard');
 
-router.route('/').post(validator.faqQuestionValidator, controller.question)
+router.route('/').post(validator.faqQuestionValidator, controller.question).get(controller.AllFAQ)
 
 router.route('/:id').put(authGuard, roleGuard("ADMIN"), validator.faqValidator, controller.answer).post(authGuard, roleGuard("ADMIN"), controller.FAQ)
 
