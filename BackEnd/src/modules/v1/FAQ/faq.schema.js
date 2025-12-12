be1de2f25f5e0d000399b0ab155d8a0c874ab5d2
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-12 12:26:59 +0330

    refactor(FAQ): add FAQ schema validation

--- BackEnd/src/modules/v1/FAQ/faq.schema.js
+++ BackEnd/src/modules/v1/FAQ/faq.schema.js
@@ -2,9 +2,14 @@
 
 const Joi = require('joi');
 
-module.exports = Joi.object().keys({
+module.exports.question = Joi.object().keys({
     name: Joi.string().max(50).required(),
     email: Joi.string().email().required(),
+    title: Joi.string().max(50).required(),
+    description: Joi.string().max(300).required(),
+})
+
+module.exports.FAQ = Joi.object().keys({
     title: Joi.string().max(50).required(),
     description: Joi.string().max(300).required(),
 })
