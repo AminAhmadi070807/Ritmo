1909b06eb9236b2f015f1bbe135edd3ec7eb7d25
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-12 09:54:44 +0330

    feat(FAQ): add faq model

---
+++ BackEnd/src/modules/v1/FAQ/faq.model.js
@@ -0,0 +1,27 @@
+"use strict"
+
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('FAQ', schema)
