72eab0925753a7bc2b726bdd4aff9e00cdff1247
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-12 09:34:49 +0330

    refactor(faq): update faq.js

--- BackEnd/public/scripts/faq/FAQ.js
+++ BackEnd/public/scripts/faq/FAQ.js
@@ -1,21 +1,21 @@
 "use strict";
 
-const questionRitmoPlus = document.querySelectorAll("#questions-ritmo-plus");
+const questionRitmoPlus = document.querySelectorAll("#question-ritmo-plus");
+
 
 questionRitmoPlus.forEach((question) => {
     question.addEventListener("click", () => {
-    if (document.querySelector(".ritmo-questions")) {
-        question.classList.add("bg-transparent-1/75");
-        question.classList.remove("ritmo-questions");
-        question.querySelector("#answer").classList.add("hidden");
-        question.querySelector("#arrowDown").classList.remove("rotate-180");
-    } else {
-        question.classList.remove("bg-transparent-1/75");
-        question.classList.add("ritmo-questions");
-        question.querySelector("#answer").classList.remove("hidden");
-        question.querySelector("#arrowDown").classList.add("rotate-180");
-    }
+        if (!question.querySelector("p.hidden")) {
+            question.classList.add("bg-transparent-1/75");
+            question.classList.remove("ritmo-questions");
+            question.querySelector("#answer").classList.add("hidden");
+            question.querySelector("#arrowDown").classList.remove("rotate-180");
+        }
+        else {
+            question.classList.remove("bg-transparent-1/75");
+            question.classList.add("ritmo-questions");
+            question.querySelector("#answer").classList.remove("hidden");
+            question.querySelector("#arrowDown").classList.add("rotate-180");
+        }
   });
 });
-
-  
