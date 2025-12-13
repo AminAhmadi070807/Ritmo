c36bae16550fb2816cd92d1b0a5f4b513033600e
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-13 10:18:41 +0330

    refactor(music): update create client

--- BackEnd/public/scripts/settings/create/music.js
+++ BackEnd/public/scripts/settings/create/music.js
@@ -7,6 +7,17 @@
         refresh.status !== 200 && (location.href = '/auth/send')
     }
     catch (error) {
-        console.log(error)
+        return model('error', error.message)
     }
 })()
+
+window.addEventListener('click', async () => {
+    try {
+        const refresh = await fetch('/api/v1/auth/refresh')
+
+        refresh.status !== 200 && (location.href = '/auth/send')
+    }
+    catch (error) {
+        return model('error', error.message)
+    }
+})
