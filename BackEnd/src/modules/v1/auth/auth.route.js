5857758eb0c9bdcae24497d70e6be4964f21191e
Amin Ahmadi <aminahmadi070807@gmail.com>
2025-12-14 16:19:28 +0330

    chore: update authorized client

--- BackEnd/src/modules/v1/auth/auth.route.js
+++ BackEnd/src/modules/v1/auth/auth.route.js
@@ -2,7 +2,6 @@
 
 const express = require('express')
 const router = express.Router()
-const passport = require('passport')
 
 const controller = require('./auth.controller.js')
 const validator = require('../../../middlewares/validate.middleware')
